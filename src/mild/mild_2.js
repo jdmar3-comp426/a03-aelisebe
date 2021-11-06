2c225d00bfad7012e2754013dfd8ed81ecc8bfdc
Anna Beaver <aelisebe@live.unc.edu>
2021-11-06 19:55:07 -0400

    try fix dest again

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -51,7 +51,8 @@ export function identifyArray(array) {
  obj now does not contain the `password` field
  */
 export function removeKey(object, key) {
-   delete object[key];
+   const {key, ...rest} = object;
+   return rest;
 }
 
 /**
