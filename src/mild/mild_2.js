5e88b351ff9d2bad1d48b5988e794705c0c97210
Anna Beaver <aelisebe@live.unc.edu>
2021-11-06 20:48:47 -0400

    removekeys attempt 1

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -99,5 +99,9 @@ export function removeKeyNonDestructive(object, key) {
  * @return {*} The object with its keys removed.
  */
 export function removeKeys(object, keyList) {
-
+   for(let i=0; i<keyList.length; i++){
+      let curKey = keyList[i];
+      delete object[curKey];
+   }
+   return object;
 }
