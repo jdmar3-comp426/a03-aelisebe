a1b5e13250f0b0186ae1dca6f39f1248d79c45e0
Anna Beaver <aelisebe@live.unc.edu>
2021-11-06 20:20:35 -0400

    fix non dest

--- src/mild/mild_2.js
+++ src/mild/mild_2.js
@@ -72,7 +72,7 @@ export function removeKey(object, key) {
  If only `removeKeyNonDestructive` was called, nothing would have changed.
  */
 export function removeKeyNonDestructive(object, key) {
-   const {toRem, ...rest} = object
+   const {key, ...rest} = object
    return rest
 }
 
