9b259cf5e06217d22f2bbaa26a47d683a6466278
Anna Beaver <aelisebe@live.unc.edu>
2021-11-13 14:08:24 -0500

    try fix key medium2

--- src/medium/medium_2.js
+++ src/medium/medium_2.js
@@ -127,8 +127,8 @@ for(let i=0; i<yearArr.length; i++){
 
 const yearObj = {};
 for(var key in arrByYear){
-    yearObj[key]={key: {"hybrid":{"city":getStatistics(arrByYear[key].hybrid.city)['mean'], "highway": getStatistics(arrByYear[key].hybrid.highway)['mean']}, "notHybrid": {"city": getStatistics(arrByYear[key].notHybrid.city)['mean'], "highway": getStatistics(arrByYear[key].notHybrid.highway)['mean']}}}
-}
+    yearObj[key]={"hybrid":{"city":getStatistics(arrByYear[key].hybrid.city)['mean'], "highway": getStatistics(arrByYear[key].hybrid.highway)['mean']}, "notHybrid": {"city": getStatistics(arrByYear[key].notHybrid.city)['mean'], "highway": getStatistics(arrByYear[key].notHybrid.highway)['mean']}}}
+
 
 
 var model = mpg_data.map(a => ({"make": a.make,"model_number": a.model_number,"hybrid": a.hybrid}));
